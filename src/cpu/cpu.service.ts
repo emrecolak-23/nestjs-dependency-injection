880d354274e9da827019c40add5f6630ffb6eea2
Emre Çolak <emrecolak@Emre.home>
2022-11-13 15:24:42 +0300

    more on di between modules

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,5 +4,11 @@ import { PowerService } from 'src/power/power.service';
 
 @Injectable()
 export class CpuService {
-    constructor(public powerService: PowerService) {}
+    constructor(private powerService: PowerService) {}
+
+    compute(a: number, b: number) {
+        console.log('Drawing 10 watts of power from Power Service')
+        this.powerService.powerSupply(10)
+        return a + b
+    }
 }
